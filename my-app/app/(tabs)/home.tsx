2225d509955f235b47db52141e5d83139b94bdd6
Wesleyllr <wesleylampier@gmail.com>
2024-11-21 14:11:49 -0300

    Iniciado Janela de pesquisa

--- my-app/app/(tabs)/home.tsx
+++ my-app/app/(tabs)/home.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image } from 'react-native'
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import SearchInput from '@/components/searchinput'
+
 
 const Home = () => {
+  const [searchValue, setSearchValue] = useState(''); // Estado para o campo de busca
+
+
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary'>
+      <FlatList 
+      data={[{id: 1},{id: 2},{id: 3}]}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({item}) => (
+        <Text className='text-3xl text-white'> {item.id}</Text>
+      )}
+      ListHeaderComponent={() => (
+        <View className='my-6 px-6 space-y-6'>
+          <View className='justify-between items-start flex-row mb-6'>
+            <View>
+              <Text className='font-pmedium text-sm text-gray-100'>
+                Bem vindo
+              </Text>
+              <Text className='text-2xl font-psemibold text-white'>
+                Wesleyllr
+              </Text>
+            </View>
+            <View className='mt-1.5'>
+              <Image source={images.logoSmall}
+              className='w-9 h-10'
+              resizeMode='contain'
+              />
+            </View>
+          </View>
+
+          <SearchInput
+              title="Search" // Título do campo (apenas semântica, se usado no futuro)
+              value={searchValue} // Valor do campo (controlado pelo estado)
+              handleChangeText={setSearchValue} // Função para atualizar o valor
+              placeholder="Buscar algo..." // Placeholder para exibição
+            />
+        </View>
+      )}
+      />
+    </SafeAreaView>
   )
 }
 
---
+++ my-app/components/SearchInput.tsx
@@ -0,0 +1,52 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react';
+import { icons, images } from '@/constants';
+import { Platform } from 'react-native';
+
+type SearchInputProps = {
+    title: string; // Título do campo
+    value: string; // Valor do campo
+    handleChangeText: (text: string) => void; // Função para manipular a entrada
+    otherStyles?: string; // Estilos adicionais (opcional)
+    placeholder?: string;
+    keyboardType?: string; // Tipo de teclado (opcional)
+  };
+
+
+  const SearchInput: React.FC<SearchInputProps> = ({
+    title, 
+    value, 
+    handleChangeText, 
+    otherStyles,
+    placeholder,
+    keyboardType = 'default'
+  }) => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+  return (
+
+
+        <View className='border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl
+        focus:border-secondary items-center flex-row space-x-4'>
+            <TextInput 
+                className='text-base mt-0.5 text-white flex-1 font-pregular '
+                value={value}
+                placeholder={placeholder}
+                placeholderTextColor="#7b7b8b"
+                onChangeText={handleChangeText}
+                secureTextEntry={title === 'Password' && !showPassword}
+            />
+            <TouchableOpacity>
+                <Image source={icons.search}
+                    className='w-5 h-5'
+                    resizeMode='contain'
+                />
+            </TouchableOpacity>
+        </View>
+  )
+}
+
+export default SearchInput
+
+
